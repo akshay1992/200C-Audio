3348984572f01e3b35d61886e1eeffef4fc0b75a
akshaycadambi <akshay19.92@gmail.com>
2015-06-07 18:51:13 -0700

    Added channel switcher pre-processor

--- ofApp.cpp
+++ ofApp.cpp
@@ -3,21 +3,31 @@
 int xRes = 400;
 int yRes = 400;
 
+// #define USING_4CHANNELS
 
 //--------------------------------------------------------------
 void ofApp::setup(){
   phase = 0;
-  ofSoundStreamSetup(4, 0); // 2 output channels (stereo), 0 input channels
+
+#ifdef USING_4CHANNELS
+  ofSoundStreamSetup(4, 0); // 4 output channels, 0 input channels
+#else
+  ofSoundStreamSetup(2, 0); // 2 output channels, 0 input channels
+#endif
+
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
 
 	int xPos, yPos;
+
+	// *** MOUSE MUTEX ***
 	mouseMutex.lock();
 	xPos = mouseX;
 	yPos = mouseY;
 	mouseMutex.unlock();
+	// *** MOUSE MUTEX ***
 		
 	if(xPos > xRes)
 		xPos = xRes-1;
@@ -29,12 +39,16 @@ void ofApp::update(){
 	else if(xPos < 0)
 		yPos = 0;
 	
+    // *** GAIN MUTEX START ***
 	gainMutex.lock();
 	gainFR = (float)xPos/xRes;
 	gainFL = 1-(float)xPos/xRes;
+#ifdef USING_4CHANNELS
 	gainRR = (float)xPos/xRes;
 	gainRL = 1-(float)xPos/xRes;
+#endif
 	gainMutex.unlock();
+	// *** GAIN MUTEX END ***
 }
 //--------------------------------------------------------------
 void ofApp::draw(){
@@ -43,22 +57,28 @@ void ofApp::draw(){
 //--------------------------------------------------------------
 void ofApp::audioOut( float * output, int bufferSize, int nChannels ) {
 
-  static float gFL , gFR, gRL, gRR;
-	
+  static float gFL , gFR;
+#ifdef USING_4CHANNELS
+  static float gRL, gRR;
+#endif	
 
+  // *** GAIN MUTEX START ***
   gainMutex.lock();
   gFL = gainFL;
   gFR = gainFR;
   gainMutex.unlock();
+  // *** GAIN MUTEX END ***
 
   //cout << "                " << gFL << " " << gFR << endl;
 
   for(int i = 0; i < bufferSize * nChannels; i += 2) {
     float sample = sin(phase); // generating a sine wave sample
-    output[i] = gFL*sample; // writing to the left channel
-    output[i+1] = gFR*sample; // writing to the right channel
+    output[i] = gFL*sample; 
+    output[i+1] = gFR*sample; 
+#ifdef USING_4CHANNELS    
     output[i+2] = gRL*sample;
 	output[i+3] = gRR*sample;  
+#endif
 	phase += 0.01;
   }
 }
@@ -83,8 +103,13 @@ void ofApp::keyReleased(int key){
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y){
+
+	// *** MOUSE MUTEX START ***
+	mouseMutex.lock();
 	mouseX = x;
 	mouseY = y;
+	mouseMutex.unlock();
+	// ** MOUSE MUTEX END ***
 
 }
 
